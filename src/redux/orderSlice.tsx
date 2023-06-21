96bc1a3d19600c71aaf4a5440a5a4e64a27f429a
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2023-06-21 15:41:30 +0300

    update url

--- src/redux/orderSlice.tsx
+++ src/redux/orderSlice.tsx
@@ -7,7 +7,7 @@ interface OrderState {
 }
 
 export const fetchOrders = createAsyncThunk("orders/fetch", async () => {
-  const response = await fetch("http://localhost:8888/");
+  const response = await fetch("http://localhost:3000/orders");
   const data = await response.json();
   return data;
 });
